95b3735d552e63984a98f1d8fa4d47fb1fd7b112
Juiceee <lbehr@student.42.fr>
2023-12-09 14:29:03 +0100

    remove truc null global

--- Include/global.h
+++ Include/global.h
@@ -17,39 +17,4 @@ typedef enum e_bool
 	True
 }	t_bool;
 
-/*typedef	struct s_game_chara
-{
-	void	*fecaface;
-	void	*fecadroite;
-	void	*fecagauche;
-	void	*fecados;
-}	t_game_chara;
-
-typedef	struct s_game_obj
-{
-	void			*sortie;
-	t_game_chara	feca;
-	void			*dofus;
-	int				nbdofus;
-	void			*mur;
-	void			*empty;
-}	t_game_obj;
-
-typedef	struct s_game_mesure
-{
-	int			width;
-	int			height;
-	int			ligne;
-	size_t		colonne;
-}	t_game_mesure;
-
-typedef struct s_game
-{
-	void			*mlx;
-	void			*win;
-	t_game_mesure	game_mesure;
-	char			**area;
-	t_game_obj		game_obj;
-}	t_game;*/
-
 #endif
